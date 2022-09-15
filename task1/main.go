336e2f29d36dac27d277b9b009bb050f70cfd6dd
kabulovvv <kabulov50@gmail.com>
2022-09-15 18:26:32 +0500

    add full task

--- task1/main.go
+++ task1/main.go
@@ -2,8 +2,18 @@ package main
 
 import "fmt"
 
+// считайте что fmt уже импортирован и main объявлен
+func test(x1 *int, x2 *int) {
+	var a int = *x1 * *x2
+	fmt.Println(a)
+	// здесь пишите ваш код
+}
+
 func main() {
 
-	fmt.Println("hello world")
+	var a int
+	var b int
+	fmt.Scanln(&a, &b)
+	test(&a, &b)
 
 }
